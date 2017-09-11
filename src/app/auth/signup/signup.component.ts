d162e0991804c8c3a7a31ec7e773c391f1bad1a4
Irene Canuti <irene.canuti@gmail.com>
2017-09-11 17:43:44 +0100

    Add reactive form for signup

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  // reactive form (data-driven)
+  signupForm: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.signupForm = new FormGroup({
+      username: new FormControl(null, Validators.required),
+      email: new FormControl(null, [
+        Validators.required,
+        Validators.pattern("^[_a-z0-9]+(\.[_a-z0-9]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$")
+      ]),
+      password: new FormControl(null, Validators.required)
+    });
+  }
+
+  onSubmit(){
+    this.signupForm.reset();
   }
 
 }
