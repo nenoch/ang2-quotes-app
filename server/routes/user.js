7742ba01658c51b1ef045697206506d66ef34c12
Irene Canuti <irene.canuti@gmail.com>
2017-09-11 19:10:42 +0100

    Add json web token for signin in the backend

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../../models/user.js');
 
@@ -22,6 +23,36 @@ router.post('/', function(req, res, next){
       obj: result
     });
   });
+});
+
+router.post('/login', function(req, res, next){
+  User.findOne({email: req.body.email}), function(err, user) {
+    if (err) {
+      return res.status(500).json({
+        title:'Error occured',
+        error: err
+      });
+    }
+    if (!user) {
+      return res.status(401).json({
+        title: 'Login failed',
+        error: {message:'Invalid credentials'}
+      });
+    }
+    if (!bcrypt.compareSync(req.body.password, user.password)) {
+      return res.status(401).json({
+        title: 'Login failed',
+        error: {message:'Invalid credentials'}
+      });
+    }
+    // store the user in the token
+    var token = jwt.sign({user: user}, 'secret', {expiresIn: 7200});
+    res.status(200).json({
+      message: 'Logged in',
+      token:token,
+      userId: user._id
+    });
+  }
 });
 
 module.exports = router;
