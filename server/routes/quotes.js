12f034dafaa7861641463e31f99c5205a1673f78
Irene Canuti <irene.canuti@gmail.com>
2017-09-13 17:08:12 +0100

    Add jwt verification on quote.js route (backend) to avoid unauthenticated users posting etc.

--- server/routes/quotes.js
+++ server/routes/quotes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 const Quote = require('../../models/quote.js')
 
 router.get('/', function(req, res, next){
@@ -9,6 +10,19 @@ router.get('/', function(req, res, next){
     }
     res.status(200).json({message: "Success", obj: quotes})
   });
+});
+
+// if the use has not a valid token cannot access the following routes
+router.use('/', function(req,res,next){
+  jwt.verify(req.query.token, 'secret', function(err, decoded){
+    if (err) {
+      res.status(401).json({
+        title: "Not authenticated user",
+        error: err
+      });
+    }
+    next();
+  })
 });
 
 module.exports = router;
